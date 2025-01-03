42f27ee7a3c978657f109410a0222da8175c0bcc
Pranali <malkarpg@gmail.com>
2025-01-03 15:58:53 +0530

    added aws s3 for image upload

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -10,19 +10,24 @@ const {
   handleGetUnpublishedBlogs,
 } = require('../controllers/blog');
 const { authMiddleware, adminMiddleware } = require('../middleware/auth');
+
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 const multer = require('multer');
 const sharp = require('sharp')
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
 const router = Router();
 
 router.get('/', handleGetBlogs);
 router.get('/tags', handleGetTags);
 router.post('/tags', authMiddleware, adminMiddleware, handleCreateTag);
 router.get('/unpublished', authMiddleware, adminMiddleware, handleGetUnpublishedBlogs);
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => cb(null, './uploads'),
-//   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
-// });
 
 const storage = multer.memoryStorage();
 
@@ -34,25 +39,38 @@ const multerFilter = (req, file, cb) => {
   }
 }
 
-const upload = multer({ storage, fileFilter: multerFilter });
+const upload = multer({ storage: storage, fileFilter: multerFilter  });
 
-const resizeUserPhoto = (req, res, next) => {
+const resizeUserPhoto = async (req, res, next) => {
   if (!req.file) return next();
 
-  req.file.filename = `${Date.now()}-${req.file.originalname}`;
-
-  sharp(req.file.buffer)
-    // .resize(500, 500)
-    // .toFormat('jpeg')
+  // Resize the image
+  const resizedImageBuffer = await sharp(req.file.buffer)
+    // .resize(500, 500) // Resize to 500x500
     .jpeg({ quality: 90 })
-    .toFile(`public/img/${req.file.filename}`);
+    .toBuffer();
 
-  next();
-}
+  // Upload the resized image to S3
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `public/img/${Date.now().toString()}-${req.file.originalname}`,
+    Body: resizedImageBuffer,
+    ACL: 'public-read',
+    ContentType: 'image/jpeg',
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    req.file.location = data.Location; // S3 URL
+    next();
+  });
+};
 
 router.get('/:id', handleGetBlogById);
 
-router.post('/', authMiddleware, adminMiddleware, upload.single('coverImageURL'),resizeUserPhoto, handleCreateBlog);
+router.post('/', authMiddleware, adminMiddleware, upload.single('coverImageURL'), resizeUserPhoto, handleCreateBlog);
 router.put('/:id', authMiddleware, adminMiddleware,upload.single('coverImageURL'),resizeUserPhoto, handleUpdateBlog);
 router.delete('/:id', authMiddleware, adminMiddleware, handleDeleteBlog);
 
